2f5a94cea170d96908c5919f3aed241195cf07b7
VagTsiats <vagtsiats12@gmail.com>
2024-09-17 11:26:16 +0300

    ekf_slam_step implementation with state sizes

--- ekf-slam.py
+++ ekf-slam.py
@@ -1,15 +1,6 @@
 import numpy as np
 
-# conventions
-# _t-1
-# t_
-
-
-# miu : [[x,   y,   th]
-#        [m1x, m1y, s1]
-#        [m2x, m2y, s2]
-#        [m1x, m1y, s1]
-#        [m1x, m1y, s1]].T
+# miu : [x, y, th, lm1x, lm1y, lm2x, lm2y, ... ].T
 # sigma :
 # f  : motion model
 # Jf : motion model derivative
@@ -20,63 +11,72 @@ import numpy as np
 # R  : motion noise
 # Q  : observation noise
 
+MH_DIST_TH = 2.0  # Threshold of Mahalanobis distance for data association.
+STATE_SIZE = 3  # State size [x,y,th]
+LM_SIZE = 2  # LM state size [x,y]
 
-def ekf_slam(miu_, sigma_, u_, z_, N_, R_, Q_):
+
+def ekf_slam_step(miu_, sigma_, u_, z_, R_, Q_):
+
+    N_ = int((miu_.shape[0] - STATE_SIZE) / LM_SIZE)
+
     # prediction
-    Fx = np.hstack((np.eye(3), np.zeros((3, 3 * N_))))
+    Fx = np.hstack((np.eye(3), np.zeros((3, LM_SIZE * N_))))
 
     miu_bar = miu_ + Fx.T @ f(miu_[:, 0], u_)
 
     Gt = np.eye(3) + Fx.T @ Jf(miu_[:, 0], u_) @ Fx
-    sigma_bar = Gt @ sigma_ @ Gt.T + R_
 
-    for i in range(len(z_)):
-        new_landmark_miu = np.zeros((3, 1))
-        r_i = z_[i, 0]
-        ph_i = z_[i, 1]
-        s_i = z_[i, 2]
-        new_landmark_miu[0, 0] = miu_bar[1, 0] + r_i * np.cos(miu_bar[0, 0] + ph_i)
-        new_landmark_miu[1, 0] = miu_bar[2, 0] + r_i * np.sin(miu_bar[0, 0] + ph_i)
-        new_landmark_miu[2, 0] = s_i
+    sigma_bar = Gt @ sigma_ @ Gt.T + Fx.T @ R_ @ Fx
+
+    # correction
+    for i in range(z_.shape[0]):
+
+        new_landmark_miu = np.zeros((2, 1))
+        new_landmark_miu[0, 0] = miu_bar[0, 0] + z_[i, 0] * np.cos(miu_bar[2, 0] + z_[i, 1])
+        new_landmark_miu[1, 0] = miu_bar[1, 0] + z_[i, 0] * np.sin(miu_bar[2, 0] + z_[i, 1])
 
         mah_dist = np.zeros((1, N_ + 1))
-        for k in range(N_ + 1):
-            z_expected_k = h(miu_bar[:, 0], miu_bar[:, k])
+        for k in range(N_):
+            z_expected_k = h(miu_bar[:STATE_SIZE, 0], miu_bar[STATE_SIZE + k * LM_SIZE : STATE_SIZE + (k + 1) * LM_SIZE, 0])
 
-            Fxk = np.zeros(6, 3(N_ + 3))
-            Fxk[:3, :3] = np.eye(3)
-            Fxk[3:, 3 + 3 * k : 6 + 3 * k] = np.eye(3)
+            Fxk = np.zeros(STATE_SIZE + LM_SIZE, LM_SIZE * N_ + STATE_SIZE)
+            Fxk[:STATE_SIZE, :STATE_SIZE] = np.eye(STATE_SIZE)
+            Fxk[STATE_SIZE:, STATE_SIZE + k * LM_SIZE : STATE_SIZE + (k + 1) * LM_SIZE] = np.eye(LM_SIZE)
 
-            Hk = H(miu_bar[:, 0], miu_bar[:, k]) @ Fxk
+            Hk = H(miu_bar[:STATE_SIZE, 0], miu_bar[STATE_SIZE + k * LM_SIZE : STATE_SIZE + (k + 1) * LM_SIZE, 0]) @ Fxk
 
             psi_k = Hk @ sigma_bar @ Hk.T + Q_
 
             # Mahalanobis Distance
             mah_dist[k] = (z_[i, :].T - z_expected_k).T @ np.linalg.pinv(psi_k) @ (z_[i, :].T - z_expected_k)
 
-        mah_dist[N_ + 1] = mahalanobis_distance_threshold
+        mah_dist[N_] = MH_DIST_TH
 
         j = np.argmin(mah_dist)  # ML correspondence selection
 
         N_ = np.max(N_, j)
-
+        # create new landmark
         if N_ == j:
-            miu_bar = np.hstack(miu_bar, new_landmark_miu)
+            miu_bar = np.vstack(miu_bar, new_landmark_miu)
 
             sigma_bar = np.vstack(
-                (np.hstack((sigma_bar, np.zeros((sigma_bar.shape[0], 3)))), np.hstack((np.zeros((3, sigma_bar.shape[1])), np.eye(3))))
+                (
+                    np.hstack((sigma_bar, np.zeros((sigma_bar.shape[0], LM_SIZE)))),
+                    np.hstack((np.zeros((LM_SIZE, sigma_bar.shape[1])), np.eye(LM_SIZE))),
+                )
             )
 
-        Fxj = np.zeros(6, 3(N_ + 3))
-        Fxj[:3, :3] = np.eye(3)
-        Fxj[3:, 3 + 3 * j : 6 + 3 * j] = np.eye(3)
+        Fxj = np.zeros(STATE_SIZE + LM_SIZE, LM_SIZE * N_ + STATE_SIZE)
+        Fxj[:STATE_SIZE, :STATE_SIZE] = np.eye(STATE_SIZE)
+        Fxj[STATE_SIZE:, STATE_SIZE + j * LM_SIZE : STATE_SIZE + (j + 1) * LM_SIZE] = np.eye(LM_SIZE)
 
-        Hj = H(miu_bar[:, 0], miu_bar[:, j]) @ Fxj
+        Hj = H(miu_bar[:STATE_SIZE, 0], miu_bar[STATE_SIZE + k * LM_SIZE : STATE_SIZE + (k + 1) * LM_SIZE, 0]) @ Fxj
         psi_j = Hj @ sigma_bar @ Hj.T + Q_
 
         K = sigma_bar @ Hj.T @ np.linalg.pinv(psi_j)
 
-        z_expected_j = h(miu_bar[:, 0], miu_bar[:, j])
+        z_expected_j = h(miu_bar[:STATE_SIZE, 0], miu_bar[STATE_SIZE + k * LM_SIZE : STATE_SIZE + (k + 1) * LM_SIZE, 0])
 
         miu_bar = miu_bar + K @ (z_[i, :] - z_expected_j)
         sigma_bar = (np.eye(np.shape(sigma_bar)) - K @ Hj) @ sigma_bar
